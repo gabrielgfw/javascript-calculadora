7b7d14ecb0200d8fabc71f73fade374e0c4543d4
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-08-25 16:58:05 -0300

    calcontroller initialize settings

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -4,6 +4,19 @@ class CalcController {
         // _ = private attributes. //
         this._displayCalc = "0";
         this._currentDate;
+        this.initialize();
+    }
+
+    initialize() {
+        // selecting objects from the html file. //
+        let displayCalcEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+
+        // innerHTML = changes the value inside the HTML tag. //
+        displayCalcEl.innerHTML = "1234";
+        dateEl.innerHTML = "20/02/2020"
+        timeEl.innerHTML = "12:30"
     }
 
     // return displayCalc value. //
