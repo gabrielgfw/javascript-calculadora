1e0091fc1e8b6be8d4d19e3c0137a23383897ea6
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-08-31 08:55:58 -0300

    add event listener to all buttons

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -4,10 +4,15 @@ class CalcController {
         // _ = private attributes. //
         // selecting objects from the html file. //
         this._locale = "pt-BR"
+        this._dateFormtConfig = {
+            day: "2-digit",
+            month: "long",
+            year: "numeric"
+        }
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora");
-
+        this.initButtonsEvents();
         this.initialize();
     }
 
@@ -20,9 +25,43 @@ class CalcController {
         }, 1000);
     }
 
+    // takes the element, all the events that we want to apply by eventListener. //
+    addEventListenerAll(element, events, fn) {
+        // this function split some string, using the parameter as the point of split. // 
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn);
+        });
+    }
+
+    // add events for all buttons. //
+    initButtonsEvents() {
+        // select all 'g' tags inside id buttons and parts. //
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+        buttons.forEach((btn, index) => {
+
+            this.addEventListenerAll(btn, 'click drag', e => {
+                // returning the class name of the current object when the same it's clicked. //
+                // notice that this replace function, removes the 'btn-' text inside each element's name. //
+                // this will make easy to recognize which button has beend clicked. //
+                console.log(btn.className.baseVal.replace("btn-", ""));
+            });
+
+            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
+
+                btn.style.cursor = 'pointer';
+            });
+
+
+        })
+
+
+    }
+
+
     setDisplayDateTime() {
         // display current date and time
-        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, this._dateFormtConfig);
         this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
     }
 
