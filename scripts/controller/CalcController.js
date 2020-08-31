356194a5310f124a10e72ba9df1f705c390a1089
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-08-31 09:35:22 -0300

    some fix on buttons config

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -12,6 +12,7 @@ class CalcController {
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora");
+        this._operation = [];
         this.initButtonsEvents();
         this.initialize();
     }
@@ -33,6 +34,63 @@ class CalcController {
         });
     }
 
+    clearAll() {
+        this._operation = [];
+    }
+
+    clearEntry() {
+        this._operation.pop();
+    }
+
+    addOperation(value) {
+        this._operation.push(value);
+        console.log(this._operation);
+    }
+
+    setError() {
+        this.displayCalc = "Error";
+    }
+
+    // sets all buttons actions. //
+    execBtn(value) {
+        switch(value) {
+
+            case 'ac' :
+                this.clearAll();
+                break;
+            case 'ce' :
+                this.clearEntry();
+                break;
+            case 'soma' :
+                break;
+            case 'subtracao' :
+                break;
+            case 'divisao' :
+                break;
+            case 'multiplicacao' :
+                break;
+            case 'porcento' :
+                break;
+            case 'igual' :
+                break;
+            
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+
+
+        }
+    }
+
     // add events for all buttons. //
     initButtonsEvents() {
         // select all 'g' tags inside id buttons and parts. //
@@ -44,7 +102,9 @@ class CalcController {
                 // returning the class name of the current object when the same it's clicked. //
                 // notice that this replace function, removes the 'btn-' text inside each element's name. //
                 // this will make easy to recognize which button has beend clicked. //
-                console.log(btn.className.baseVal.replace("btn-", ""));
+                let textBtn = btn.className.baseVal.replace("btn-", "");
+
+                this.execBtn(textBtn);
             });
 
             this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
